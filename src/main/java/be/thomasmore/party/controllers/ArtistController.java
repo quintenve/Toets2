c0624f05344db38bf7ac3b19a428440a20cff807
verapeeters-thomasmore <vera.peeters@thomasmore.be>
2020-07-23 09:11:15 +0200

    artist list and details pages

--- src/main/java/be/thomasmore/party/controllers/ArtistController.java
+++ src/main/java/be/thomasmore/party/controllers/ArtistController.java
@@ -1,12 +1,38 @@
 package be.thomasmore.party.controllers;
 
+import be.thomasmore.party.model.Artist;
 import be.thomasmore.party.repositories.ArtistRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Optional;
 
 @Controller
 public class ArtistController {
     @Autowired
     private ArtistRepository artistRepository;
+
+    @GetMapping("/artistlist")
+    public String artistList(Model model) {
+        Iterable<Artist> artists = artistRepository.findAll();
+        model.addAttribute("artists", artists);
+        return "artistlist";
+    }
+
+    @GetMapping({ "/artistdetails/{id}", "/artistdetails"})
+    public String artistDetails(Model model,
+                                @PathVariable(required = false) Integer id) {
+        if (id == null) return "artistdetails";
+
+        Optional<Artist> optionalArtist = artistRepository.findById(id);
+        //noinspection OptionalIsPresent
+        if (optionalArtist.isPresent()) {
+            model.addAttribute("artist", optionalArtist.get());
+        }
+        return "artistdetails";
+    }
 
 }
