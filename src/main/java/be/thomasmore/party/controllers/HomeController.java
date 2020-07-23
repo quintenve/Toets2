f12ec2613db548431b034dd0aa731715834e5661
verapeeters-thomasmore <vera.peeters@thomasmore.be>
2020-07-23 09:11:15 +0200

    chapter 1: add variabele in Request Handler

--- src/main/java/be/thomasmore/party/controllers/HomeController.java
+++ src/main/java/be/thomasmore/party/controllers/HomeController.java
@@ -1,14 +1,19 @@
 package be.thomasmore.party.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-    @GetMapping("/")
-    public String home() {
+
+    @GetMapping({"/", "/home"})
+    public String home(Model model) {
+        int myCalculatedValue = 34 * 62;
+        model.addAttribute("myCalculatedValue", myCalculatedValue);
         return "home";
     }
+
     @GetMapping("/about")
     public String about() {
         return "about";
