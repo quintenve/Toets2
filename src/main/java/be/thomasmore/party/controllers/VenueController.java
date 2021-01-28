08af7f94b57cd34b3e52c7cd1677b89790c9616b
verapeeters-thomasmore <vera.peeters@thomasmore.be>
2021-01-28 16:55:31 +0100

    add logger and receive the input field in Request Handler
    (cherry picked from commit 798b289f74afe2af3b7cb8b6372667023479328e)

--- src/main/java/be/thomasmore/party/controllers/VenueController.java
+++ src/main/java/be/thomasmore/party/controllers/VenueController.java
@@ -2,21 +2,27 @@ package be.thomasmore.party.controllers;
 
 import be.thomasmore.party.model.Venue;
 import be.thomasmore.party.repositories.VenueRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Optional;
 
 @Controller
 public class VenueController {
+    private Logger logger = LoggerFactory.getLogger(VenueController.class);
+
     @Autowired
     private VenueRepository venueRepository;
 
     @GetMapping({"/venuelist"})
     public String venueList(Model model) {
+        logger.info("venueList");
         Iterable<Venue> venues = venueRepository.findAll();
         long nrOfVenues = venueRepository.count();
         model.addAttribute("venues", venues);
@@ -26,7 +32,9 @@ public class VenueController {
     }
 
     @GetMapping({"/venuelist/filter"})
-    public String venueListWithFilter(Model model) {
+    public String venueListWithFilter(Model model,
+                                      @RequestParam(required=false) Integer minCapacity) {
+        logger.info(String.format("venueListWithFilter -- min=%d", minCapacity));
         Iterable<Venue> venues = venueRepository.findAll();
         long nrOfVenues = venueRepository.count();
         model.addAttribute("venues", venues);
