4383521dd2b63f6d219ac25c20a8e2badcda870d
verapeeters-thomasmore <vera.peeters@thomasmore.be>
2021-01-28 18:15:55 +0100

    filter artists: simple keyword

--- src/main/java/be/thomasmore/party/controllers/ArtistController.java
+++ src/main/java/be/thomasmore/party/controllers/ArtistController.java
@@ -3,26 +3,51 @@ package be.thomasmore.party.controllers;
 import be.thomasmore.party.model.Artist;
 import be.thomasmore.party.model.Venue;
 import be.thomasmore.party.repositories.ArtistRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
 import java.util.Optional;
 
 @Controller
 public class ArtistController {
+    final private Logger logger = LoggerFactory.getLogger(ArtistController.class);
+
     @Autowired
     private ArtistRepository artistRepository;
 
     @GetMapping("/artistlist")
     public String artistList(Model model) {
-        Iterable<Artist> artists = artistRepository.findAll();
+        List<Artist> artists = artistRepository.findAllBy();
         model.addAttribute("artists", artists);
+        model.addAttribute("nrOfArtists", artists.size());
+        model.addAttribute("showFilters", false);
         return "artistlist";
     }
 
+    @GetMapping("/artistlist/filter")
+    public String artistListWithFilter(Model model,
+                                       @RequestParam(required = false) String keyword) {
+        logger.info(String.format("artistListWithFilter -- keyword=%s",keyword));
+
+        List<Artist> artists;
+        if (keyword != null)
+            artists = artistRepository.findByArtistNameContainingIgnoreCase(keyword);
+        else
+            artists = artistRepository.findAllBy();
+
+        model.addAttribute("artists", artists);
+        model.addAttribute("nrOfArtists", artists.size());
+        model.addAttribute("showFilters", true);
+        model.addAttribute("keyword", keyword);
+        return "artistlist";
+    }
     @GetMapping({ "/artistdetails/{id}", "/artistdetails"})
     public String artistDetails(Model model,
                                 @PathVariable(required = false) Integer id) {
--- src/main/java/be/thomasmore/party/repositories/ArtistRepository.java
+++ src/main/java/be/thomasmore/party/repositories/ArtistRepository.java
@@ -5,13 +5,14 @@ import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
 
+import java.util.List;
+
 public interface ArtistRepository extends CrudRepository<Artist, Integer> {
+    List<Artist> findAllBy();
+    List<Artist> findByArtistNameContainingIgnoreCase(String keyword);
 
     Optional<Artist> findFirstByIdLessThanOrderByIdDesc(Integer id);
-
     Optional<Artist> findFirstByOrderByIdDesc();
-
     Optional<Artist> findFirstByIdGreaterThanOrderByIdAsc(Integer id);
-
     Optional<Artist> findFirstByOrderByIdAsc();
 }
