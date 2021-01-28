8de41e15acaa3d3f80fbfb8bd9633ecc79522bcf
verapeeters-thomasmore <vera.peeters@thomasmore.be>
2021-01-28 20:03:34 +0100

    JPQL: simple jpql for venue filters

--- src/main/java/be/thomasmore/party/repositories/VenueRepository.java
+++ src/main/java/be/thomasmore/party/repositories/VenueRepository.java
@@ -1,21 +1,33 @@
 package be.thomasmore.party.repositories;
 
 import be.thomasmore.party.model.Venue;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface VenueRepository extends CrudRepository<Venue, Integer> {
     Iterable<Venue> findByOutdoor(boolean isOutdoor);
+
     Iterable<Venue> findByIndoor(boolean isIndoor);
-    List<Venue> findByCapacityBetween(int min, int max);
-    List<Venue> findByCapacityGreaterThan(int min);
+
     List<Venue> findAllBy();
 
+    @Query("SELECT v FROM Venue v WHERE :min <= v.capacity AND v.capacity <= :max")
+    List<Venue> findByCapacityBetween(@Param("min") int min, @Param("max") int max);
+
+    @Query("SELECT v FROM Venue v WHERE :min <= v.capacity")
+    List<Venue> findByCapacityGreaterThan(@Param("min") int min);
+
     Optional<Venue> findFirstByIdLessThanOrderByIdDesc(Integer id);
+
     Optional<Venue> findFirstByOrderByIdDesc();
+
     Optional<Venue> findFirstByIdGreaterThanOrderByIdAsc(Integer id);
+
     Optional<Venue> findFirstByOrderByIdAsc();
+
 
 }
