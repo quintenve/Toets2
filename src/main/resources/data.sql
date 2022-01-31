11d954aa3a9fb1a2a4c3663697250461f7c7ae27
verapeeters-thomasmore <vera.peeters@thomasmore.be>
2022-01-31 15:47:23 +0100

    entity object for Venue: insert data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into VENUE (ID, VENUE_NAME, LINK_MORE_INFO, CAPACITY,
+                   FOOD_PROVIDED, INDOOR, OUTDOOR, FREE_PARKING_AVAILABLE,
+                   CITY, DISTANCE_FROM_PUBLIC_TRANSPORT_IN_KM)
+values (1, 'De Club', 'https://transitm.mechelen.be/de-club',
+        150, false, true, false, true, 'Mechelen', 2);
