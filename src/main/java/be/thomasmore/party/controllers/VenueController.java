6cbb005e36abe023546de042b2783b4fb58571c6
verapeeters-thomasmore <vera.peeters@thomasmore.be>
2022-01-31 15:29:37 +0100

    show more details for 1 venue (POJO)

--- src/main/java/be/thomasmore/party/controllers/VenueController.java
+++ src/main/java/be/thomasmore/party/controllers/VenueController.java
@@ -7,11 +7,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class VenueController {
+
     @GetMapping("/venuedetails")
     public String venuedetails(Model model) {
-        Venue venue = new Venue("BoesjKammeree", "https://www.facebook.com/boesjkammeree/");
+        Venue venue = new Venue("Boesj", "https://www.facebook.com/boesjkammeree/", 100, false, true, false, false, "Mechelen", 1);
         model.addAttribute("venue", venue);
         return "venuedetails";
     }
-
 }
