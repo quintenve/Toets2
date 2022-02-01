009fb374c16ed3c8171270cb9f1882d990de876a
verapeeters-thomasmore <vera.peeters@thomasmore.be>
2022-02-01 11:31:17 +0100

    get venue from database

--- src/main/java/be/thomasmore/party/controllers/VenueController.java
+++ src/main/java/be/thomasmore/party/controllers/VenueController.java
@@ -1,17 +1,28 @@
 package be.thomasmore.party.controllers;
 
 import be.thomasmore.party.model.Venue;
+import be.thomasmore.party.repositories.VenueRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Optional;
+
 @Controller
 public class VenueController {
+    @Autowired
+    private VenueRepository venueRepository;
 
     @GetMapping("/venuedetails")
     public String venuedetails(Model model) {
-        Venue venue = new Venue("Boesj", "https://www.facebook.com/boesjkammeree/", 100, false, true, false, false, "Mechelen", 1);
-        model.addAttribute("venue", venue);
+        //Venue venue = new Venue("Boesj", "https://www.facebook.com/boesjkammeree/", 100, false, true, false, false, "Mechelen", 1);
+        Optional<Venue> venueFromDb = venueRepository.findById(1);
+        //noinspection OptionalIsPresent
+        if (venueFromDb.isPresent()) {
+            model.addAttribute("venue", venueFromDb.get());
+        }
         return "venuedetails";
     }
+
 }
