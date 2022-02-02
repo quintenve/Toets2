dfc0c4b7267935721f89ea00bf6e93fcf3424bde
verapeeters-thomasmore <vera.peeters@thomasmore.be>
2022-02-02 10:59:43 +0100

    complex queries: Party partylist

---
+++ src/main/java/be/thomasmore/party/controllers/PartyController.java
@@ -0,0 +1,25 @@
+package be.thomasmore.party.controllers;
+
+import be.thomasmore.party.model.Party;
+import be.thomasmore.party.repositories.PartyRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PartyController {
+    private final Logger logger = LoggerFactory.getLogger(PartyController.class);
+
+    @Autowired
+    private PartyRepository partyRepository;
+
+    @GetMapping("/partylist")
+    public String partyList(Model model) {
+        Iterable<Party> parties = partyRepository.findAll();
+        model.addAttribute("parties", parties);
+        return "partylist";
+    }
+}
